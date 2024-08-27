1667fda1b15153edb2c5903e76f29e6527717b2e
Azalea Colburn <azalea.colburn@autodesk.com>
2024-08-27 15:24:03 -0700

    new init :)

---
+++ .zshrc
@@ -0,0 +1,23 @@
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# File System Aliases
+alias ll="eza -a -l"
+alias th="z"
+alias nd="mkdir"
+
+# Git Aliases
+alias c="git commit -a"
+alias push="git push"
+alias pull="git pull"
+alias fetch="git fetch"
+alias checkout="git checkout"
+
+# Neovim Aliases
+alias e="nvim"
+
+GPG_TTY=$(tty)
+export GPG_TTY
+
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
---
+++ nvim/lua/azalea/lazy.lua
@@ -0,0 +1,65 @@
+local root = vim.fn.expand("~")
+
+-- set stdpaths to use .repro
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+	vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
+
+-- bootstrap lazy
+local lazypath = root .. "/plugins/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("lazy").setup({ { import = "azalea.plugins" }, { import = "azalea.plugins.lsp" } })
+
+vim.cmd("colorscheme tokyonight-storm")
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.opt.laststatus = 0
+vim.opt.cursorline = false
+vim.opt.swapfile = false
+
+-- Relative + current line numbers
+vim.wo.relativenumber = true
+vim.wo.number = true
+
+-- Offwhite line numbers
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#c7c9d6" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#c7c9d6" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#c7c9d6" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#c7c9d6" })
+-- Gray comments
+vim.api.nvim_set_hl(0, "Comment", { fg = "#9fa2bd" })
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = "NONE" })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	command = "setlocal nocursorline",
+})
+-- Stray Keymaps
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "l", "b", opts)
+vim.keymap.set("n", "L", "B", opts)
+vim.keymap.set("n", "b", "l", opts)
+vim.keymap.set("n", "B", "L", opts)
+
+vim.keymap.set("n", "<leader>o", "<C-o>", opts)
+vim.keymap.set("n", "<leader>e", ":Explore<cr>", opts)
